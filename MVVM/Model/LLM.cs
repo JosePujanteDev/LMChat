ffbd1e4b37b30d6a0fd99ec623d5c0632d30a522
JPujii <josepujante77@gmail.com>
2024-05-02 20:29:50 +0200

    Commit v1.0

---
+++ MVVM/Model/LLM.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLMChat.MVVM.Model
+{
+    class LLM
+    {
+        public async Task<string> PostAPIAsync(string message, string model)
+        {
+            try
+            {
+                var request = new RestRequest("/chat/completions", Method.Post);
+                request.AddJsonBody(new
+                {
+                    model = model,
+                    messages = new List<object>
+                    {
+                        new { role = "system", content = "Always answer in Spanish." },
+                        new { role = "user", content = message }
+                    },
+                    temperature = 0.7
+                });
+
+                var response = await RestClientInstance.ExecuteAsync(request);
+
+                if (response.IsSuccessful)
+                {
+                    dynamic data = JsonConvert.DeserializeObject(response.Content);
+                    string result = data.choices[0].message.content;
+                    return result;
+                }
+                else
+                {
+                    throw new Exception("Error al hacer la solicitud: " + response.ErrorMessage);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al hacer la solicitud: " + e.Message);
+            }
+        }
+
+        private static string _baseUrl = "http://localhost:1234/v1";
+        public static string BaseUrl
+        {
+            get { return _baseUrl; }
+            set
+            {
+                _baseUrl = value;
+                _restClientInstance = new RestClient(_baseUrl);
+            }
+        }
+
+        private static RestClient _restClientInstance;
+        private static readonly object _lock = new object();
+        private static RestClient RestClientInstance
+        {
+            get
+            {
+                if (_restClientInstance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (_restClientInstance == null)
+                        {
+                            _restClientInstance = new RestClient(_baseUrl);
+                        }
+                    }
+                }
+                return _restClientInstance;
+            }
+        }
+    }
+}
--- MVVM/ViewModel/MainWindowVM.cs
+++ MVVM/ViewModel/MainWindowVM.cs
@@ -1,25 +1,31 @@
 ﻿using LLMChat.Core;
 using LLMChat.MVVM.Model;
 using System.Collections.ObjectModel;
-using System.ComponentModel.Design;
 
 namespace LLMChat.ViewModel
 {
     class MainWindowVM : ObservableObject
     {
-        public ObservableCollection<MessageModel> Messages { get; set; }
+        public ObservableCollection<MessageModel> MessagesDolphin { get; set; }
+        public ObservableCollection<MessageModel> MessagesSolar { get; set; }
+        public ObservableCollection<MessageModel> MessagesAuto { get; set; }
+
+
         public ObservableCollection<ContactModel> Contacts { get; set; }
+        private LLM _llm; 
+
 
         // Commandos
         public RelayCommand SendCommand { get; set; }
+        private string modelMessage;
 
         private ContactModel _selectedContact;
 
         public ContactModel SelectedContact
         {
             get { return _selectedContact; }
-            set 
-            { 
+            set
+            {
                 _selectedContact = value;
                 OnPropertyChanged();
             }
@@ -40,77 +46,179 @@ namespace LLMChat.ViewModel
 
         public MainWindowVM()
         {
-            Messages = new ObservableCollection<MessageModel>();
+            MessagesDolphin = new ObservableCollection<MessageModel>();
+            MessagesSolar = new ObservableCollection<MessageModel>();
+            MessagesAuto = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
 
-            SendCommand = new RelayCommand(o =>
-            {
-                Messages.Add(new MessageModel
-                {
-                    Message = Message,
-                    FirstMessage = false
-                });
+            _llm = new LLM(); 
 
-                Message = "";
+            SendCommand = new RelayCommand(async o =>
+            {
+                if (SelectedContact.Username == "Dolphin")
+                {
+                    LLM.BaseUrl = "http://localhost:5678/v1"; 
+
+                    MessagesDolphin.Add(new MessageModel
+                    {
+                        Username = "Username",
+                        UsernameColor = "#4DB31F",
+                        ImageSource = "https://biblioteca.acropolis.org/wp-content/uploads/2014/12/verde.png",
+                        Message = Message,
+                        Time = DateTime.Now,
+                        IsNativeOrigin = true
+                    });
+
+                    modelMessage = Message;
+                    Message = "";
+
+                    string response = await _llm.PostAPIAsync(modelMessage, "TheBloke/dolphin-2.6-mistral-7B-GGUF");
+
+                    MessagesDolphin.Add(new MessageModel
+                    {
+                        Username = "Dolphin",
+                        UsernameColor = "#409AFF",
+                        ImageSource = "https://cdn-uploads.huggingface.co/production/uploads/63111b2d88942700629f5771/ldkN1J0WIDQwU4vutGYiD.png",
+                        Message = response,
+                        Time = DateTime.Now,
+                        IsNativeOrigin = true
+                    });
+                }
+                else if (SelectedContact.Username == "Solar")
+                {
+                    LLM.BaseUrl = "http://localhost:1234/v1";
+
+                    MessagesSolar.Add(new MessageModel
+                    {
+                        Username = "Username",
+                        UsernameColor = "#4DB31F",
+                        ImageSource = "https://biblioteca.acropolis.org/wp-content/uploads/2014/12/verde.png",
+                        Message = Message,
+                        Time = DateTime.Now,
+                        IsNativeOrigin = true
+                    });
+
+                    modelMessage = Message;
+                    Message = "";
+
+                    string response = await _llm.PostAPIAsync(modelMessage, "TheBloke/SOLAR-10.7B-Instruct-v1.0-uncensored-GGUF");
+
+                    MessagesSolar.Add(new MessageModel
+                    {
+                        Username = "Solar",
+                        UsernameColor = "#DFCC27",
+                        ImageSource = "https://www.hardware-corner.net/wp-content/uploads/llm/images/Solar-logo.jpg",
+                        Message = response,
+                        Time = DateTime.Now,
+                        IsNativeOrigin = true
+                    });
+                }
+                else if (SelectedContact.Username == "Autochat")
+                {
+                    MessagesAuto.Add(new MessageModel
+                    {
+                        Username = "Username",
+                        UsernameColor = "#4DB31F",
+                        ImageSource = "https://biblioteca.acropolis.org/wp-content/uploads/2014/12/verde.png",
+                        Message = Message,
+                        Time = DateTime.Now,
+                        IsNativeOrigin = true
+                    });
+
+                    modelMessage = Message;
+                    Message = "";
+
+                    while(modelMessage != ".")
+                    {
+                        LLM.BaseUrl = "http://localhost:5678/v1";
+                        string response = await _llm.PostAPIAsync(modelMessage, "TheBloke/dolphin-2.6-mistral-7B-GGUF");
+
+                        MessagesAuto.Add(new MessageModel
+                        {
+                            Username = "Dolphin",
+                            UsernameColor = "#409AFF",
+                            ImageSource = "https://cdn-uploads.huggingface.co/production/uploads/63111b2d88942700629f5771/ldkN1J0WIDQwU4vutGYiD.png",
+                            Message = response,
+                            Time = DateTime.Now,
+                            IsNativeOrigin = true
+                        });
+
+                        modelMessage = response;
+
+                        LLM.BaseUrl = "http://localhost:1234/v1";
+                        response = await _llm.PostAPIAsync(modelMessage, "TheBloke/SOLAR-10.7B-Instruct-v1.0-uncensored-GGUF");
+
+                        MessagesAuto.Add(new MessageModel
+                        {
+                            Username = "Solar",
+                            UsernameColor = "#DFCC27",
+                            ImageSource = "https://www.hardware-corner.net/wp-content/uploads/llm/images/Solar-logo.jpg",
+                            Message = response,
+                            Time = DateTime.Now,
+                            IsNativeOrigin = true
+                        });
+                    }
+                    
+                }
+
             });
 
-            Messages.Add(new MessageModel
+            // Mensaje inicial Dolphin
+            MessagesDolphin.Add(new MessageModel
             {
                 Username = "Dolphin",
                 UsernameColor = "#409AFF",
                 ImageSource = "https://cdn-uploads.huggingface.co/production/uploads/63111b2d88942700629f5771/ldkN1J0WIDQwU4vutGYiD.png",
-                Message = "Los delfines dominaran el mundo",
+                Message = "Hola, ¿que tal estas?",
                 Time = DateTime.Now,
                 IsNativeOrigin = false,
                 FirstMessage = true
             });
 
-            for (int i = 0; i < 3; i++)
-            {
-                Messages.Add(new MessageModel
-                {
-                    Username = "Dolphin",
-                    UsernameColor = "#409AFF",
-                    ImageSource = "https://cdn-uploads.huggingface.co/production/uploads/63111b2d88942700629f5771/ldkN1J0WIDQwU4vutGYiD.png",
-                    Message = "Los delfines dominaran el mundo jaja",
-                    Time = DateTime.Now,
-                    IsNativeOrigin = false,
-                    FirstMessage = false
-                });
-            }
-
-            for (int i = 0; i < 4; i++)
-            {
-                Messages.Add(new MessageModel
-                {
-                    Username = "Solar",
-                    UsernameColor = "#409AFF",
-                    ImageSource = "https://www.hardware-corner.net/wp-content/uploads/llm/images/Solar-logo.jpg",
-                    Message = "Evaporare toda el agua del planeta muajaja",
-                    Time = DateTime.Now,
-                    IsNativeOrigin = true,
-                });
-            }
-
-            Messages.Add(new MessageModel
+            // Mensaje inicial Solar
+            MessagesSolar.Add(new MessageModel
             {
                 Username = "Solar",
-                UsernameColor = "#409AFF",
+                UsernameColor = "#DFCC27",
                 ImageSource = "https://www.hardware-corner.net/wp-content/uploads/llm/images/Solar-logo.jpg",
-                Message = "Maldito delfin",
+                Message = "Soy el sol",
                 Time = DateTime.Now,
-                IsNativeOrigin = true,
+                IsNativeOrigin = false,
+                FirstMessage = true
             });
 
-            for (int i = 0; i < 4; i++)
+            // Mensaje inicial Autochat
+            MessagesAuto.Add(new MessageModel
             {
-                Contacts.Add(new ContactModel
-                {
-                    Username = $"Delfin {i}",
-                    ImageSource = "https://cdn-uploads.huggingface.co/production/uploads/63111b2d88942700629f5771/ldkN1J0WIDQwU4vutGYiD.png",
-                    Messages = Messages
-                });
-            }
+                Username = "Autochat",
+                UsernameColor = "#63DF27",
+                ImageSource = "https://getchat.app/assets/img/whatsapp-large.png",
+                Message = "Escribe para iniciar.",
+                Time = DateTime.Now,
+                IsNativeOrigin = false,
+                FirstMessage = true
+            });
+
+            Contacts.Add(new ContactModel
+            {
+                Username = "Dolphin",
+                ImageSource = "https://cdn-uploads.huggingface.co/production/uploads/63111b2d88942700629f5771/ldkN1J0WIDQwU4vutGYiD.png",
+                Messages = MessagesDolphin
+            });
+
+            Contacts.Add(new ContactModel
+            {
+                Username = "Solar",
+                ImageSource = "https://www.hardware-corner.net/wp-content/uploads/llm/images/Solar-logo.jpg",
+                Messages = MessagesSolar
+            });
+            
+            Contacts.Add(new ContactModel
+            {
+                Username = "Autochat",
+                ImageSource = "https://getchat.app/assets/img/whatsapp-large.png",
+                Messages = MessagesAuto
+            });
 
         }
     }
